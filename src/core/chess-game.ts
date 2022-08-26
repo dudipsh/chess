279767346ce6d22648a23d95f417e84e67ec5b5e
Dudi Partush <dudipsh@gmail.com>
2022-08-26 14:26:40 +0300

    fix move validation

--- src/core/chess-game.ts
+++ src/core/chess-game.ts
@@ -6,30 +6,11 @@ export class ChessGame extends Chess {
     return this;
   }
 
-  randomMove = () => {
+  randomMove = (): string => {
     const possibleMoves = this.moves();
-    if (possibleMoves.length === 0) return;
+    if (possibleMoves.length === 0) return "";
     const randomIdx = Math.floor(Math.random() * possibleMoves.length);
     this.move(possibleMoves[randomIdx]);
-  };
-
-  isValidMove = (move: any) => {
-    const piece = this.get(move.from);
-
-    if (piece?.type !== "p") {
-      return false;
-    }
-
-    if (piece.color !== this.turn()) {
-      return false;
-    }
-
-    if (!["1", "8"].some((it) => move.to.endsWith(it))) {
-      return false;
-    }
-
-    return this.moves({ square: move.from, verbose: true })
-      .map((it) => it.to)
-      .includes(move.to);
+    return possibleMoves[randomIdx];
   };
 }
